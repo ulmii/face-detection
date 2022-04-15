2f7b0eb84f3c795d9b13d99d1c1749d3b8e6350e
Konrad Ulman <veradopl@gmail.com>
2022-04-16 01:17:47 +0200

    Add fastface

--- src/benchmark/utils/__init__.py
+++ src/benchmark/utils/__init__.py
@@ -1,4 +1,4 @@
 #! /usr/bin/env python3
 
-from .main import load_aflw, tf_to_image_faces, get_box_id, run_detection
+from .main import tf_to_image_faces, get_box_id, run_detection
 from .tsvhandle import TsvHandle
--- src/benchmark/utils/main.py
+++ src/benchmark/utils/main.py
@@ -1,8 +1,6 @@
 #! /usr/bin/env python3
 
 import sys
-import os.path
-import sqlite3
 
 from ..models.face import Face
 from ..models.box import Box
@@ -11,9 +9,6 @@ from ..tools import *
 from ..models import *
 
 from datetime import datetime
-
-images_home = '../../AFLW/images'
-query_string = "SELECT image_id, filepath, Faces.face_id, x, y, w, h FROM FaceImages, Faces, FaceRect WHERE FaceImages.file_id = Faces.file_id AND Faces.face_id = FaceRect.face_id"
 
 face_id = 0
 box_id = 0
@@ -46,43 +41,6 @@ def tf_to_image_faces(tf_obj):
     image_faces = ImageFaces(get_image_id(), tf_obj['image/filename'].numpy().decode("utf-8"), faces_with_boxes, img.numpy())
     
     return image_faces
-
-def load_aflw(limit = None):
-    global box_counter
-
-    conn = sqlite3.connect('../src/aflw.sqlite')
-    c = conn.cursor()
-    
-    query = query_string
-    if limit is not None:
-        query += ' LIMIT {}'.format(limit)
-
-    image_data_dict = {}
-
-    for row in c.execute(query):
-        file_path = str(row[1])
-        input_path = images_home + '/' + file_path
-        
-        if(os.path.isfile(input_path) == True):
-            image_id = row[0]
-            face_id = row[2]
-            face_x = row[3]
-            face_y = row[4]
-            face_w = row[5]
-            face_h = row[6]
-            
-            face = Face(face_id, Box(box_id = box_id, x1 = face_x, y1 = face_y, w = face_w, h = face_h))
-            box_id += 1
-            
-            if image_id in image_data_dict:
-                image_data_dict[image_id].add_face(face)
-            else:
-                img_face = ImageFaces(image_id, input_path)
-                img_face.add_face(face)
-                
-                image_data_dict[image_id] = img_face
-
-    return image_data_dict.values()
 
 def run_detection(tsv_handle, samples, detector: Detector, cv2_filter = None, use_width_height = False, display_data = False, display_filter = None):
     total_data = len(samples)
